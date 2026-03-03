daf6c33c956f2f41dc62d1b75aafa7dac4d032f8
aoaochan <josejbeck99@yahoo.com>
2026-03-03 12:50:07 +0900

    solved the bug: internal server error

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,13 +1,13 @@
 import { jsxRenderer } from 'hono/jsx-renderer';
-import { Link, ViteClient } from 'vite-ssr-components/hono';
+import { ViteClient } from 'vite-ssr-components/hono';
 
 export const renderer = jsxRenderer(({ children }) => {
   return (
     <html>
       <head>
         <ViteClient />
-        <Link href="/src/style.css" rel="stylesheet" />
-        <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link href="/style.css" rel="stylesheet" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <script src="/js/htmx@2.0.8.min.js"></script>
         <title>aoaochan</title>
       </head>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from 'vite'
 import ssrPlugin from 'vite-ssr-components/plugin'
 
 export default defineConfig({
-  plugins: [cloudflare(), ssrPlugin()]
+  plugins: [cloudflare(), ssrPlugin()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/index.tsx'
+      }
+    }
+  }
 })
